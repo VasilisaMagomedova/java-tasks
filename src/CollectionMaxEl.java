2aef5fb241c785e906bbc872428854c1c1925e2f
Vasilisa@ <vv.magomedova00@gmail.com>
2024-12-03 14:09:38 +0700

    Added solutions 4-5 for collection tasks

--- src/CollectionMaxEl.java
+++ src/CollectionMaxEl.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CollectionMaxEl {
+import java.util.*;
+
+public class CollectionMaxEl {
+//    Напишите метод, который принимает список чисел List<Integer> и возвращает максимальный элемент списка.
+//    Если список пустой, выбросьте исключение IllegalArgumentException
+    public static int findMaxEl(List<Integer> numbersList) {
+        try {
+            return Collections.max(numbersList);
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("Передан пустой список чисел!");
+        }
+    }
 }
--- src/CollectionReversed.java
+++ src/CollectionReversed.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class CollectionReversed {
+import java.util.*;
+
+public class CollectionReversed {
+//    Напишите метод, который принимает список чисел List<Integer> и возвращает новый список,
+//    содержащий те же элементы в обратном порядке
+    public static List<Integer> reverseList(List<Integer> listOfNumbers) {
+        Collections.reverse(listOfNumbers);
+        return listOfNumbers;
+    }
 }
