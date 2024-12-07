483483855fccfc2a93cceb6fbc3a0e6a7d7968a9
Vasilisa@ <vv.magomedova00@gmail.com>
2024-12-07 13:54:57 +0700

    Solved task Comparator

---
+++ src/Lambdas_And_Functional_Interfaces/Person.java
@@ -0,0 +1,23 @@
+package Lambdas_And_Functional_Interfaces;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + age;
+    }
+}
